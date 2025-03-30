a35cc81bc18f6f5edb2360aaf0cf37b78fcdb78e
Shun Cyrel <caseres.shun@gmail.com>
2025-03-31 00:55:12 +0800

    Update urls

--- backend/myproject/urls.py
+++ backend/myproject/urls.py
@@ -23,7 +23,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myapp.urls')),  
+    path('api/todos/', include('myapp.urls')),  
 ]
 
 if not settings.DEBUG:
