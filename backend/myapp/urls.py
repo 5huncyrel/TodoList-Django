0e097fefbe4227a43ec9575c253dae35b185d982
Shun Cyrel <caseres.shun@gmail.com>
2025-03-30 22:34:13 +0800

    Update backend

--- backend/myapp/urls.py
+++ backend/myapp/urls.py
@@ -3,9 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('fetch/', views.task_list, name='task-fetch'),  # Fetching all tasks
-    path('create/', views.task_create, name='task-create'),  # Create a task
-    path('<int:pk>/fetch/', views.task_detail, name='task-fetch-single'),  # Fetch a single task by ID
-    path('<int:pk>/update/', views.task_update, name='task-update'),  # Update a task by ID
-    path('<int:pk>/delete/', views.task_delete, name='task-delete'),  # DELETE a task by ID
+    path('api/todos/fetch/', views.task_list, name='task-fetch'),  # Fetching all tasks
+    path('api/todos/create/', views.task_create, name='task-create'),  # Create a task
+    path('api/todos/<int:pk>/fetch/', views.task_detail, name='task-fetch-single'),  # Fetch a single task by ID
+    path('api/todos/<int:pk>/update/', views.task_update, name='task-update'),  # Update a task by ID
+    path('api/todos/<int:pk>/delete/', views.task_delete, name='task-delete'),  # DELETE a task by ID
 ]
