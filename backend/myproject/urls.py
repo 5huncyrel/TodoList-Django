92d3393b5357c8d996f1826598ff2bc7ef5cd17e
Shun Cyrel <caseres.shun@gmail.com>
2025-03-30 21:17:02 +0800

    Update urls.py

--- backend/myproject/urls.py
+++ backend/myproject/urls.py
@@ -18,8 +18,11 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import redirect
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('myapp.urls')),
+    path('', lambda request: redirect('api/todos/fetch/')),  
+    path('api/todos/', include('myapp.urls')),  
 ]
+
